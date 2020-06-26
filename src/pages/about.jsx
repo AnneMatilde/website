4b1c95594ee5979e01e41db99f9586dc8c98acd1
Simen A. W. Olsen <cobraz@cobraz.no>
2020-06-26 23:06:11 +0200

    chore: first commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            Her kommer om meg.
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            Dette er forsiden.
+        </div>
+    )
+}
+
+export default index
