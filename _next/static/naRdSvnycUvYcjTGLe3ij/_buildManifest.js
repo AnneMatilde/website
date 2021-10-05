dac578fcbc4e178aef1967fecd773a11df54aa3b
cobraz <cobraz@cobraz.no>
2021-10-05 19:50:08 +0000

    Deploying to gh-pages from  @ b6fbb7f144ec61c1cc7c62d45116c1e1e496d5a5 🚀

---
+++ _next/static/naRdSvnycUvYcjTGLe3ij/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-37f37c473707a38e9846.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/about":["static/chunks/pages/about-60c9251080e483a320cd.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
